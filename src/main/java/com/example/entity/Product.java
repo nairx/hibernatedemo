81b3a63ce7f768a16c9d56c71b031430c40f6e71
nairx <nairxprv@gmail.com>
2026-08-12 16:34:43 +0530

    updated

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -1,8 +1,36 @@
 package com.example.entity;
-
-import jakarta.persistence.Entity;
-
+import jakarta.persistence.*;
 @Entity
+// @Table(name="healthProducts")
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name="product_seq",initialValue = 1001,allocationSize = 1)
+    private Long id;
+    private String name;
+    private int price;
+    public Product(){
+    }
+    public Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
