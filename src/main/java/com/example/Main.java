b551e067faab6e648ef4315d8b8477436674d243
nairx <nairxprv@gmail.com>
2026-08-13 13:46:58 +0530

    updated

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,6 @@
 package com.example;
+import com.example.entity.*;
+
 import jakarta.persistence.*;
 
 public class Main {
@@ -6,7 +8,15 @@ public class Main {
         System.out.println("My Application!");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPersistenceUnit");
         EntityManager em = emf.createEntityManager();
+        System.out.println("My Application");
         //actions;
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //Product product = new Product("Product 1",100);
+        //em.persist(product);
+         User user = new User("John","john@gmail.com","pass1234");
+        em.persist(user);
+        tx.commit();
         em.close();
         emf.close();
     }
